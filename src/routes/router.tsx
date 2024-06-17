c158abbd5ee2c1aba9f1a8d6f5cb97f4e664725b
Cornerstone-04 <fortunecornerstone@gmail.com>
2024-06-17 14:14:34 +0100

    feat: Implement landing page first draft

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Landing } from "../pages/landing-page";
+import { Landing } from "../pages";
 
 export const AppRoutes = () => {
   return (
